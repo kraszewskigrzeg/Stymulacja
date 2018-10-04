ab12a0c8906a07b4288631099bd3519bd3511d27
Grzegorz Kraszewski <43808089+kraszewskigrzeg@users.noreply.github.com>
2018-10-04 12:41:50 +0200

    Initial commit

---
+++ 1.py
@@ -0,0 +1,13 @@
+print("czy liczba jest większa od 5")
+x=input()
+x=float(x)
+
+if x==5 :
+   print("tak")
+else :
+    print("nie")
+if x> 5 :
+    print("większa")
+if x< 5 :
+    print("mniejsza")
+
